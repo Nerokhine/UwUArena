df8ea70753ca32fbd97a6ab4871cf971c46c10b3
Nikita Erokhine <nerokhin@uwaterloo.ca>
2020-05-18 19:25:13 -0700

    unity project

---
+++ UwUArena/Assets/Minion.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minion {
+	private int health;
+	private int attack;
+	private string name;
+	private Tribe tribe;
+	private delegate void Death();
+	private delegate void Entry();
+	private delegate void Conditional();
+	
+
+	public void IsDead() {
+		return health <= 0;
+	}
+
+	public void TakeDamage (int damage) 
+    {
+        health -= damage;
+ 
+		if (IsDead()) {
+			Death();
+		}
+
+    }
+
+	private void Initialize () {
+		Initializer.Initialize(this);
+	}
+
+	public void GiveEffect (delegate DeathEffect, delegate EntryEffect, delegate ConditionalEffect) {
+		Death = DeathEffect;
+		Entry = EntryEffect;
+		// Conditional effects will check their trigger on every "game action" that happens.
+		// A game action is an entry, death, attack, damage calculation, minion effect (including conditional effects)
+		Conditional = ConditionalEffect;
+	}
+ 
+    public void GiveStats(int attack, int health) {
+        health += health;
+		attack += attack;
+
+         if (IsDead()) {
+            Death();
+		 }
+    }
+
+}
