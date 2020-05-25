21a789adb7d2736ad65ea3b49b4b08f43eec1088
Nikita Erokhine <nerokhin@uwaterloo.ca>
2020-05-25 15:15:46 -0700

    minion data class, effects class

--- UwUArena/Assets/Scripts/Game.cs
+++ UwUArena/Assets/Scripts/Game.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Game: MonoBehaviour {
-    private static String MINIONS_CSV_FILE = "./UwUArenaMinions.csv";
     //private Player[] player;
 
 
     // Use this for initialization
 	void Start () {
-		InitializeCards();
+		MinionData.Initialize();
+        Debug.Log(MinionData.GetMinionData("Sharko").GetAttack());
 	}
 	
 	// Update is called once per frame
@@ -21,23 +21,4 @@ public class Game: MonoBehaviour {
     /*public Game() {
         InitializeCards();
     }*/
-
-    public void ParseCsv (String file) {
-        String fileData = System.IO.File.ReadAllText(file);
-        String[] lines = fileData.Split("\n"[0]);
-        bool initializedRowNames = false;
-        foreach (String line in lines) {
-            String[] lineData = (line.Trim()).Split(","[0]);
-            if (!initializedRowNames) {
-                initializedRowNames = true;
-            }
-            foreach (String record in lineData) {
-                Debug.Log(record);
-            }
-        }
-    }
-
-    public void InitializeCards() {
-        ParseCsv(MINIONS_CSV_FILE);
-    }
 }
---
+++ UwUArena/Assets/Scripts/MinionData.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MinionData {
+    private static Dictionary<string, MinionData> minionData = new Dictionary<string, MinionData>();
+    private static string MINIONS_CSV_FILE = "./UwUArenaMinions.csv";
+    private int health;
+    private int attack;
+    private int level;
+    private string name;
+    private Tribe tribe;
+    private Effect effect;
+
+    public int GetHealth() {
+        return health;
+    }
+
+    public int GetAttack() {
+        return attack;
+    }
+
+    public int GetLevel() {
+        return level;
+    }
+
+    public string GetName() {
+        return name;
+    }
+
+    public Tribe GetTribe() {
+        return tribe;
+    }
+
+    public MinionData(int level, string name, Tribe tribe, int attack, int health) {
+        this.level = level;
+        this.name = name;
+        this.tribe = tribe;
+        this.attack = attack;
+        this.health = health;
+        minionData.Add(name, this);
+    }
+
+    private static Tribe GetTribe(string tribe) {
+        switch (tribe) {
+            case "Aquatic":
+                return Tribe.Aquatic;
+            case "Pyro":
+                return Tribe.Pyro;
+            case "Forest":
+                return Tribe.Forest;
+            case "Thunder":
+                return Tribe.Thunder;
+        }
+        throw new System.ArgumentException("Invalid Tribe Input", "original");
+    }
+
+    private static void ParseCsv (string file) {
+        String fileData = System.IO.File.ReadAllText(file);
+        String[] lines = fileData.Split("\n"[0]);
+        bool initializedRowNames = false;
+        foreach (string line in lines) {
+            string[] lineData = (line.Trim()).Split(","[0]);
+            if (!initializedRowNames) {
+                initializedRowNames = true;
+            } else {
+                MinionData data = new MinionData(
+                    level:Int32.Parse(lineData[0]),
+                    name:lineData[1],
+                    tribe:GetTribe(lineData[2]),
+                    attack:Int32.Parse(lineData[3]),
+                    health:Int32.Parse(lineData[4])
+                );
+            }
+        }
+    }
+
+    public static MinionData GetMinionData(string name) {
+        return minionData[name];
+    }
+
+    public static void Initialize() {
+        ParseCsv(MINIONS_CSV_FILE);
+    }
+}
